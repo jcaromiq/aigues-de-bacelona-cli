0a317be5ab1e03310ba763a6134e03de4752fae9
Joaquin Caro <jcaromiq@gmail.com>
2023-01-16 18:39:31 +0100

    optimize release binary

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ mod domain;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let api = Api::new().await?;
-    println!(
-        "Today Consumed liters {:?}",
-        api.get_today_consumptions().await?
-    );
+    let today = api.get_today_consumptions().await?;
+    let yesterday = api.get_yesterday_consumptions().await?;
 
     println!(
-        "Yesterday Consumed liters {:?}",
-        api.get_yesterday_consumptions().await?
+        "Consumed liters\nYesterday\t\t\t: {}\nToday's provisional\t: {}",
+        yesterday, today
     );
     Ok(())
 }
